11991002f142ea2e6335c41c9844bbe36d613dcf
bananaFish001 <raj123.rs308@gmail.com>
2024-03-25 22:33:35 +0530

    learning about various parts of urls

---
+++ 08_sections_of_urls.js
@@ -0,0 +1,7 @@
+function printURLParts(urlString){
+  const urlObj = new URL(urlString)
+}
+
+
+const fantasyQuestURL = 'http://dragonslayer:pwn3d@fantasyquest.com:8080/maps?sort=rank#id'
+printURLParts(fantasyQuestURL)
