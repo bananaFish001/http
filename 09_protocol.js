8793411a860d4971efe94ae0972f3bebbd0644c6
bananaFish001 <raj123.rs308@gmail.com>
2024-03-26 00:29:53 +0530

    returning a mail protocol

---
+++ 09_protocol.js
@@ -0,0 +1,9 @@
+function getMailtoLinkForEmail(email){
+  return `mailto:slayer@fquest.app`
+}
+
+
+let email = 'slayer@fquest.app'
+console.log(`The mailto link for ${email} is: ${getMailtoLinkForEmail(email)}`)
+email = 'killer@fquest.app'
+console.log(`The mailto link for ${email} is: ${getMailtoLinkForEmail(email)}`)
