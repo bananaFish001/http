a1c63684eac732f528aa660a65d13458fb34a8b4
bananaFish001 <raj123.rs308@gmail.com>
2024-03-26 00:13:12 +0530

    just making some changes to get the specific parts i wanr

--- 08_sections_of_urls.js
+++ 08_sections_of_urls.js
@@ -1,5 +1,14 @@
 function printURLParts(urlString){
   const urlObj = new URL(urlString)
+  console.log(urlObj)
+  console.log(`protocol: ${urlObj.protocol}`)
+  console.log(`username: ${urlObj.username}`)
+  console.log(`password: ${urlObj.password}`)
+  console.log(`hostname: ${urlObj.username}`)
+  console.log(`port: ${urlObj.port}`)
+  console.log(`pathname: ${urlObj.pathname}`)
+  console.log(`search: ${urlObj.search}`)
+  console.log(`hash: ${urlObj.hash}`)
 }
 
 
